2bd2e2fa65c4ab31b55b17077b7c9177f9fc1641
clreinki <clreinki@indiana.edu>
2018-07-25 14:51:16 -0400

    Initial Upload

---
+++ StartPowershell/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using murrayju.ProcessExtensions;
+
+namespace StartPowershell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                Console.WriteLine("args is null");
+            }
+            else
+            {
+                ProcessExtensions.StartProcessAsCurrentUser(args[0]);
+            }
+            
+        }
+    }
+}
